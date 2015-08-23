d6777e014a574d9a770e5274e2e03450334d8da9
Socialery <Socialery@users.noreply.github.com>
2015-08-23 11:28:06 +0930

    Update Problem 206 - Concealed Square.py

--- Problem 206 - Concealed Square.py
+++ Problem 206 - Concealed Square.py
@@ -1,7 +1,7 @@
 
 def is_concealed_square(n):
     sq = str(n ** 2)
-    if len(sq) == 19 and sq[0] == "1" and sq[2] == "2" and sq[4] == "3" and sq[6] == "4" and sq[8] == "5" and sq[10] == "6" and sq[12] == "7" and sq[14] == "8" and sq[16] == "9" and sq[18] == "0":
+    if len(sq) == 19 and sq[::2]==list('1234567890'):
         return True
     return False
 
