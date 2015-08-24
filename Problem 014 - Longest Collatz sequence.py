bda9259641cf0582b951c03d4cd94f944d250254
kennypybot <kennypybot@gmail.com>
2015-08-24 23:07:04 +0200

    PEP 8 Compliance

--- Problem 014 - Longest Collatz sequence.py
+++ Problem 014 - Longest Collatz sequence.py
@@ -1,6 +1,6 @@
 from time import clock
-
 start = clock()
+
 
 def collatz_sequence(number):
     sequence = [number]
@@ -13,15 +13,12 @@ def collatz_sequence(number):
     return sequence
 
 
-
 longest_chain = 0
 starting_number = 0
 for i in range(1, 1000000):
     if len(collatz_sequence(i)) > longest_chain:
         longest_chain = len(collatz_sequence(i))
         starting_number = i
-
 print('The answer is:', starting_number, 'with a chain length of:', longest_chain)
-
 end = clock()
 print(end - start)
