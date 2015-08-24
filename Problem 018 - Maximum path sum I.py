a32f510f3a72698fe4bc8385798f4011a5414a6c
kennypybot <kennypybot@gmail.com>
2015-08-24 23:15:00 +0200

    PEP 8 Compliance

--- Problem 018 - Maximum path sum I.py
+++ Problem 018 - Maximum path sum I.py
@@ -1,15 +1,13 @@
 
 def read_file(a):
-    #Leest de hele file, return lijsten in een lijst
+    #Reads the whole file, returns lists in a list
     with open(a, 'r') as file:
         result = []
         for line in file:
             result.append(line.rstrip().split())
         return result
 
-"""--------------------------"""
 data = read_file("data.txt")
-print(data)
 for i in range(1, len(data))[::-1]:
     for j in range(0, len(data[i-1])):
         data[i - 1][j] = str(int(data[i - 1][j]) + max(int(data[i][j]), int(data[i][j + 1])))
