b68d46dcd5751c920c09643d7e2d00c1b66d5e02
kennypybot <kennypybot@gmail.com>
2015-08-24 18:29:19 +0200

    Added an alternative solution.

--- Problem 001 - Multiples of 3 and 5.py
+++ Problem 001 - Multiples of 3 and 5.py
@@ -3,3 +3,6 @@ for i in range(1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         answer += i
 print(answer)
+
+#Or the alternative one liner:
+print(sum([x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0]))
