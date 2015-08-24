e6bbbc036387045cabfed47290a213b5947923c1
kennypybot <kennypybot@gmail.com>
2015-08-24 23:19:19 +0200

    Small change to the code.

--- Problem 020 - Factorial digit sum.py
+++ Problem 020 - Factorial digit sum.py
@@ -1,7 +1,6 @@
-import math
-result = math.factorial(100)
-resulta = 0
+from math import factorial
+factorial_number = factorial(100)
+result = 0
+for i in str(factorial_number):
+    result += int(i)
 print(result)
-for i in str(result):
-    resulta += int(i)
-print(resulta)
