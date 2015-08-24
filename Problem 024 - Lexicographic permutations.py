9f3201e445204d07fcc3feb068c169e8e6fb1104
kennypybot <kennypybot@gmail.com>
2015-08-24 23:51:03 +0200

    Code improvement and PEP8 compliance.

--- Problem 024 - Lexicographic permutations.py
+++ Problem 024 - Lexicographic permutations.py
@@ -1,18 +1,3 @@
 from itertools import permutations
 
-def is_in_lexicographic_order(number):
-    temp = str(number)[0]
-    for i in str(number)[1:-1]:
-        if int(i) > int(temp):
-            temp = int(i)
-        else:
-            return False
-    return True
-
-answer_list = []
-list = ["0", "1", "2", "3", "4", '5', '6', '7', '8', '9']
-for i in permutations(list):
-    temp = ''.join(i)
-    answer_list.append(temp)
-
-print(sorted(answer_list)[999999])
+print(sorted([''.join(x) for x in permutations('0123456789')])[999999])
